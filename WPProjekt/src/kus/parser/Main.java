5a551b1156e06760fffa2ebaae48b67f79fd005a
KaBoKu <kk.up.krk@gmail.com>
2013-06-24 23:44:21 +0200

    Prawie koniec

--- WPProjekt/src/kus/parser/Main.java
+++ WPProjekt/src/kus/parser/Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -58,6 +60,90 @@ public class Main {
 	        }
 	        System.out.println(line);
 	        
+	        RSSYahooWether p = new RSSYahooWether();
+	        String a = new String(p.xml(line, "channel", "windows-1250").getBytes(),"windows-1250");
+	        System.out.println(a);
+	        System.out.println(YahooCodes2.getDescribe(0));
+	
+	        
 	}
+	
+	
+	
 
+}
+
+enum YahooCodes2{
+	 
+	TORNADO(0,"tornado");
+	private final String describe; 
+	private final int code;
+	private static  Map<Integer, YahooCodes2> mapEnum;
+	YahooCodes2(int code,String str){
+		describe=str;
+		this.code = code;
+	}
+	
+	static void initMap(){
+		mapEnum = new HashMap<Integer,YahooCodes2>();
+		for(YahooCodes2 yC : values() ){
+			mapEnum.put(yC.code, yC);
+		}
+	}
+	
+	public static  String getDescribe(int i){
+		initMap();		
+		return mapEnum.get(i).describe;
+	}
+/*		0 	tornado
+	1 	tropical storm
+	2 	hurricane
+	3 	severe thunderstorms
+	4 	thunderstorms
+	5 	mixed rain and snow
+	6 	mixed rain and sleet
+	7 	mixed snow and sleet
+	8 	freezing drizzle
+	9 	drizzle
+	10 	freezing rain
+	11 	showers
+	12 	showers
+	13 	snow flurries
+	14 	light snow showers
+	15 	blowing snow
+	16 	snow
+	17 	hail
+	18 	sleet
+	19 	dust
+	20 	foggy
+	21 	haze
+	22 	smoky
+	23 	blustery
+	24 	windy
+	25 	cold
+	26 	cloudy
+	27 	mostly cloudy (night)
+	28 	mostly cloudy (day)
+	29 	partly cloudy (night)
+	30 	partly cloudy (day)
+	31 	clear (night)
+	32 	sunny
+	33 	fair (night)
+	34 	fair (day)
+	35 	mixed rain and hail
+	36 	hot
+	37 	isolated thunderstorms
+	38 	scattered thunderstorms
+	39 	scattered thunderstorms
+	40 	scattered showers
+	41 	heavy snow
+	42 	scattered snow showers
+	43 	heavy snow
+	44 	partly cloudy
+	45 	thundershowers
+	46 	snow showers
+	47 	isolated thundershowers
+	3200 	not available*/
+	
+	
 }
--- WPProjekt/src/kus/parser/RSSYahooWether.java
+++ WPProjekt/src/kus/parser/RSSYahooWether.java
@@ -1,14 +1,60 @@
 package kus.parser;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 public class RSSYahooWether extends TemplateXML{
 
 	@Override
 	public String extractData(Document doc) {
 		// TODO Auto-generated method stub
-		return null;
+		String result="";
+		System.out.println(doc.getDocumentElement());
+		Node n = doc.getDocumentElement();
+		NodeList nL = doc.getElementsByTagName("*");
+		System.out.println(nL.getLength());
+		for(int i=0;i<nL.getLength();++i){
+			Node cN = nL.item(i);
+			System.out.println(cN.getNodeName());
+			Element el = (Element) cN;
+			if (el.getTagName().startsWith("yweather:wind")){
+				System.out.println("Temperatura: "+el.getAttribute("chill")+"C");
+				result+="Temperatura: "+el.getAttribute("chill")+"C"+"\n";
+				System.out.println("Prêdkoœæ kierunek: "+el.getAttribute("direction")+" Stopni");
+				result+="Prêdkoœæ kierunek: "+el.getAttribute("direction")+" Stopni"+"\n";
+				System.out.println("Prêdkoœæ wiatru: "+el.getAttribute("speed")+"km/h");
+				result+="Prêdkoœæ wiatru: "+el.getAttribute("speed")+"km/h"+"\n";
+			}
+			if (el.getTagName().startsWith("yweather:atmosphere")){
+				System.out.println("Wilgotnoœæ: "+el.getAttribute("humidity")+"%");
+				result+="Wilgotnoœæ: "+el.getAttribute("humidity")+"%"+"\n";
+				System.out.println("Widocznoœæ: "+el.getAttribute("visibility")+"km");
+				result+="Widocznoœæ: "+el.getAttribute("visibility")+"km"+"\n";
+				System.out.println("Ciœnienie: "+el.getAttribute("pressure")+"mb");
+				result+="Ciœnienie: "+el.getAttribute("pressure")+"mb"+"\n";
+			}
+			
+			if (el.getTagName().startsWith("yweather:astronomy")){
+				System.out.println("Wschód: "+el.getAttribute("sunrise"));
+				result+="Wschód: "+el.getAttribute("sunrise")+"\n";
+				System.out.println("Zachód: "+el.getAttribute("sunset"));
+				result+="Zachód: "+el.getAttribute("sunset")+"\n";
+			}
+			if (el.getTagName().startsWith("yweather:condition")){
+				System.out.println("Opis: "+el.getAttribute("text"));
+				System.out.println("Kod: "+el.getAttribute("code"));
+			}
+			
+			
+		}
+		System.out.println("system "+Charset.defaultCharset());
+		System.out.println(result);
+		return result;//element.getAttribute("city");
 	}
 
 	@Override
@@ -17,4 +63,80 @@ public class RSSYahooWether extends TemplateXML{
 		return null;
 	}
 
+}
+
+ enum YahooCodes{
+	 
+		TORNADO(0,"tornado");
+		private final String describe; 
+		private final int code;
+		private static  Map<Integer, YahooCodes> mapEnum;
+		YahooCodes(int code,String str){
+			describe=str;
+			this.code = code;
+		}
+		
+		static void initMap(){
+			mapEnum = new HashMap<Integer,YahooCodes>();
+			for(YahooCodes yC : values() ){
+				mapEnum.put(yC.code, yC);
+			}
+		}
+		
+		public static  String getDescribe(int i){
+			initMap();		
+			return mapEnum.get(i).describe;
+		}
+	
+/*		0 	tornado
+	1 	tropical storm
+	2 	hurricane
+	3 	severe thunderstorms
+	4 	thunderstorms
+	5 	mixed rain and snow
+	6 	mixed rain and sleet
+	7 	mixed snow and sleet
+	8 	freezing drizzle
+	9 	drizzle
+	10 	freezing rain
+	11 	showers
+	12 	showers
+	13 	snow flurries
+	14 	light snow showers
+	15 	blowing snow
+	16 	snow
+	17 	hail
+	18 	sleet
+	19 	dust
+	20 	foggy
+	21 	haze
+	22 	smoky
+	23 	blustery
+	24 	windy
+	25 	cold
+	26 	cloudy
+	27 	mostly cloudy (night)
+	28 	mostly cloudy (day)
+	29 	partly cloudy (night)
+	30 	partly cloudy (day)
+	31 	clear (night)
+	32 	sunny
+	33 	fair (night)
+	34 	fair (day)
+	35 	mixed rain and hail
+	36 	hot
+	37 	isolated thunderstorms
+	38 	scattered thunderstorms
+	39 	scattered thunderstorms
+	40 	scattered showers
+	41 	heavy snow
+	42 	scattered snow showers
+	43 	heavy snow
+	44 	partly cloudy
+	45 	thundershowers
+	46 	snow showers
+	47 	isolated thundershowers
+	3200 	not available*/
+	
+	
 }
