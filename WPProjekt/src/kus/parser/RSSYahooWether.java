c6fb1bf2523bcf5b8b5e2cdeaf1f23ab0badf430
KaBoKu <kk.up.krk@gmail.com>
2013-06-24 19:46:47 +0200

    Wether Parser

---
+++ WPProjekt/src/kus/parser/RSSYahooWether.java
@@ -0,0 +1,20 @@
+package kus.parser;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class RSSYahooWether extends TemplateXML{
+
+	@Override
+	public String extractData(Document doc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBaseData(NodeList nList0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
